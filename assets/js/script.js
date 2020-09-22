597f214fbd2f33c744bf3503267e92c78e178a47
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2020-09-21 21:55:50 -0700

    removed debugger

--- assets/js/script.js
+++ assets/js/script.js
@@ -356,7 +356,6 @@ function createMap() {
         $('head').children('style').remove();
     }
 
-    debugger;
     // Create the script tag, set the appropriate attributes
     var script = document.createElement('script');
     script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCv_iF_YniNOH9mI6WvJc66w5bo3_PXXCg&callback=initMap';
